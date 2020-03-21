c8772fdf1afe1cc55024cd92426d821b4b1c45db
JonathanOTU <jonathan.zhu@ontariotechu.net>
2020-03-21 14:28:02 -0400

    Finally set up gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("hello");
+
+        Scene Scene = new Scene(label);
+        primaryStage.setScene(Scene);
+        primaryStage.show();
+    }
+}
